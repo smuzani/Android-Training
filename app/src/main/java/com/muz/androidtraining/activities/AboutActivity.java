6f7f24f74a39ea4128569ba1524ed83513949d7b
Muz <smuzani@gmail.com>
2016-08-14 11:25:05 +0800

    Back/Up button

--- app/src/main/java/com/muz/androidtraining/activities/AboutActivity.java
+++ app/src/main/java/com/muz/androidtraining/activities/AboutActivity.java
@@ -2,6 +2,7 @@ package com.muz.androidtraining.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.webkit.WebView;
 
 import com.muz.androidtraining.R;
@@ -12,8 +13,14 @@ public class AboutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("About");
+
         WebView wv = (WebView) findViewById(R.id.wv);
         wv.getSettings().setJavaScriptEnabled(true);
         wv.loadUrl("http://www.itrainasia.com/");
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 }
