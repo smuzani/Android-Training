341426f9805f4a7cd39ea5be34b7b8c9ff6bd789
Muz <smuzani@gmail.com>
2016-08-15 22:29:28 +0800

    Map Activity

--- app/src/main/java/com/muz/androidtraining/activities/MainActivity.java
+++ app/src/main/java/com/muz/androidtraining/activities/MainActivity.java
@@ -47,6 +47,7 @@ public class MainActivity extends AppCompatActivity {
         links.add(new MainMenuLink("Lesson 2.1-2.2: Navigation Drawer & Back", NavDrawerActivity.class));
         links.add(new MainMenuLink("Lesson 2.3-2.5: Tabs, CardView, & Transition", TabbedActivity.class));
         links.add(new MainMenuLink("Lesson 2.6: Notification", NotificationActivity.class));
+        links.add(new MainMenuLink("Lesson 3.2: Maps", MapsActivity.class));
 //        links.add(new MainMenuLink("Lesson 2.4: CardView and RecyclerView", TabbedActivity.class));
     }
 }
