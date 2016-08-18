7d4be6a56d60d0503b048ed26bcbed669a5850b3
Muz <smuzani@gmail.com>
2016-08-18 23:20:34 +0800

    WIP: AddItemActivity

---
+++ app/src/main/java/com/muz/androidtraining/activities/FirebaseDatabase.java
@@ -0,0 +1,15 @@
+package com.muz.androidtraining.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.muz.androidtraining.R;
+
+public class FirebaseDatabase extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_firebase_database);
+    }
+}
